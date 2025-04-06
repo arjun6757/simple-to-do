a140830aa07c41b978074c2b0f81cda042df51e6
arjun31 <arjunbanerjee13@gmail.com>
2025-04-07 02:28:13 +0530

    refactor: use next/image instead of img

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,12 +3,14 @@ import { Button } from "./ui/button";
 import { Card, CardContent } from "./ui/card";
 import { createClient } from "@/utils/supabase/server";
 import { handleSignOut } from "@/app/login/actions";
+import Image from "next/image";
 
 export default async function Header() {
+	const supabase = await createClient();
 
-	const supabase = await createClient()
-
-	const { data: { user } } = await supabase.auth.getUser()
+	const {
+		data: { user },
+	} = await supabase.auth.getUser();
 
 	return (
 		<Card className="py-3 border-none shadow-none w-full px-0 font-mono">
@@ -25,24 +27,30 @@ export default async function Header() {
 					</li>
 				</ul>
 				<div className="flex flex-1 items-center justify-end space-x-2">
-          {user ?
-            (
-              <form action={handleSignOut} className="flex items-center space-x-2">
-                {/*<p>{user.email?.split("@")[0]}</p>*/}
-              <div className="flex gap-2 items-center rounded-md px-2 py-1 bg-[#f0f0f0] font-sans">
-              	<img referrerPolicy="no-referrer" src={user.user_metadata?.avatar_url} alt="avatar" className="w-8" />
-              	<p>{user.user_metadata?.full_name}</p>
-              </div>
-                <Button>Sign Out</Button>
-              </form>
-            )
-            :
-            (
-              <Button asChild className="cursor-pointer" >
-                <Link href="/login">Sign In</Link>
-              </Button>
-            )}
-        </div>
+					{user ? (
+						<form
+							action={handleSignOut}
+							className="flex items-center space-x-2"
+						>
+							{/*<p>{user.email?.split("@")[0]}</p>*/}
+							<div className="flex gap-2 items-center rounded-md px-2 py-1 bg-[#f0f0f0] font-sans">
+								<Image
+									referrerPolicy="no-referrer"
+									src={user.user_metadata?.avatar_url}
+									alt="avatar"
+									width={30}
+									height={30}
+								/>
+								<p>{user.user_metadata?.full_name}</p>
+							</div>
+							<Button>Sign Out</Button>
+						</form>
+					) : (
+						<Button asChild className="cursor-pointer">
+							<Link href="/login">Sign In</Link>
+						</Button>
+					)}
+				</div>
 			</CardContent>
 		</Card>
 	);
