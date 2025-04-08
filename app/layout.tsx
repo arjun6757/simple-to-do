03399735bad49ee6174824a74f7137f1275e4e84
arjun31 <arjunbanerjee13@gmail.com>
2025-04-08 21:42:45 +0530

    refactor: replace vh with dvh

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
     <html lang="en">
       <body
       //  sm:ml-[56px] mb-[56px] sm:mb-0 sm:w-[calc(100vw-56px)]
-        className={`${geistSans.variable} ${geistMono.variable} antialiased w-screen sm:w-[calc(100vw-56px)] sm:ml-14 h-[calc(100vh-56px)] mb-14 sm:mb-0 sm:h-screen flex flex-col-reverse sm:flex-row justify-center items-center`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased w-screen sm:w-[calc(100vw-56px)] sm:ml-14 h-[calc(100dvh-56px)] mb-14 sm:mb-0 sm:h-dvh flex flex-col-reverse sm:flex-row justify-center items-center`}
       >
         <Sidebar />
         {children}
